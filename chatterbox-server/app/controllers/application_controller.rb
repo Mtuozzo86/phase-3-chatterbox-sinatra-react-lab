02abd4f2c8daf1b63ab11abea67c3b5f03c408c6
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-12-02 13:09:23 -0500

    commit tree b68ad4637768bda6bf4d8ce4f7ac95e2cd18be10

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,26 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
+  set :default_content_type, "application/json"
+
   # Add routes
-  
+  get "/messages" do
+    list = Message.all
+    list.to_json
+  end
+
+  post "/messages" do
+    new_message = Message.create(username: params[:username], body: params[:body])
+    new_message.to_json
+  end
+
+  delete "/messages/:id" do
+    remove = Message.find(params[:id])
+    remove.destroy
+    remove.to_json
+  end
+
+  patch "/messages/:id" do
+    change = Message.find(params[:id])
+    change.update(body: params[:body])
+    change.to_json
+  end
 end
